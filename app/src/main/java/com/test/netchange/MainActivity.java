18d4a76e54296994f0b02ae722a28215cd15c1c6
zhongrui <zhongrui@021.com>
2020-06-10 18:31:56 +0800

    临时提交

--- app/src/main/java/com/test/netchange/MainActivity.java
+++ app/src/main/java/com/test/netchange/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.github.networkchange.NetChangeManager;
 import com.github.networkchange.NetChangerListener;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button bt;
     private Button btText;
+    private Button btClearText;
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         bt = findViewById(R.id.bt);
         btText = findViewById(R.id.btText);
+        tvText = findViewById(R.id.tvText);
+        btClearText = findViewById(R.id.btClearText);
+        btClearText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvText.setText("");
+            }
+        });
 
         /*使用之前注册一次,可以写在application中*/
         NetChangeManager.get().register(this);
@@ -34,12 +45,13 @@ public class MainActivity extends AppCompatActivity {
                         btText.setText("监听当前网络:GPRS");
                         bt.setText("点击手动获取当前网络");
                         Log.i("=====","====MainActivity=onConnect==GPRS"+netType);
-
+                        tvText.setText(tvText.getText()+"\nGPRS");
                         break;
                     case NetType.WIFI:
                         btText.setText("监听当前网络:WIFI");
                         bt.setText("点击手动获取当前网络");
                         Log.i("=====","====MainActivity=onConnect==WIFI"+netType);
+                        tvText.setText(tvText.getText()+"\nWIFI");
                         break;
                 }
             }
@@ -48,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 btText.setText("监听当前网络:NONE");
                 bt.setText("点击手动获取当前网络");
                 Log.i("=====","====MainActivity=onConnect====NONE");
+                tvText.setText(tvText.getText()+"\nNONE");
             }
         });
 
@@ -59,14 +72,17 @@ public class MainActivity extends AppCompatActivity {
                     case NetType.GPRS:
                         Log.i("=====","====MainActivity=getCurrentNetType====GPRS");
                         bt.setText("点击手动获取当前网络(GPRS)");
+                        tvText.setText(tvText.getText()+"\nGPRS_click");
                     break;
                     case NetType.WIFI:
                         Log.i("=====","====MainActivity=getCurrentNetType====WIFI");
                         bt.setText("点击手动获取当前网络(WIFI)");
+                        tvText.setText(tvText.getText()+"\nWIFI_click");
                     break;
                     case NetType.NONE:
                         Log.i("=====","====MainActivity=getCurrentNetType====NONE");
                         bt.setText("点击手动获取当前网络(NONE)");
+                        tvText.setText(tvText.getText()+"\nNONE_click");
                     break;
                 }
             }
